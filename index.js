5802ddf617dd580414d7af00ffc16fc289e4a8be
soumyas <so.singh1226@gmail.com>
2020-12-04 23:16:46 -0500

    update

--- index.js
+++ index.js
@@ -1,15 +1,68 @@
 // array of questions for user
-const questions = [
+var inquirer = require ("inquirer")
+var fs = require ("fs")
+var generateMarkdown = require("./utils/generateMarkdown");
 
+const questions = [
+        {
+            type: "input",
+            message: "What is the title of your project?",
+            name: "Title"
+        },
+        {
+            type: "input",
+            message: "Describe your project",
+            name: "Description"
+        },
+        {
+            type: "input",
+            message: "Write installation instructions",
+            name: "Instructions"
+        },
+        {
+            type: "input",
+            message: "Write usage information",
+            name: "Information"
+        },
+        {
+            type: "input",
+            message: "What are the contribution guidelines",
+            name: "Guidelines"
+        },
+        {
+            type: "input",
+            message: "Write test instructions",
+            name: "TestInstructions"
+        },
+        {
+            type: "input",
+            message: "What's your gitHub username?",
+            name: "GitHub"
+        },
+        {
+            type: "input",
+            message: "Enter your email",
+            name: "Email"
+        },
+        
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (error) {
+        if (error) throw error;
+        console.log("success");
+    })
 }
 
 // function to initialize program
 function init() {
+    inquirer.prompt (questions)
+    .then (function (response){
+        console.log (response)
 
+        writeToFile("readMe.md", generateMarkdown(response))
+    })
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,42 @@
+// const html = `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+//     <img src=`${https://img.shields.io/badge/LICENSE-mit-green/}`>
+// </body>
+// </html>`;
+
+LICENSE: MIT
+
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# Title: 
+  ${data.Title},
 
-`;
+  # Description of the Project:
+  ${data.Description},
+
+  # Installation Instructions:
+  ${data.Instructions},
+
+  # Usage Information:
+  ${data.Information},
+
+  # Contribution Guidelines:
+  ${data.Guidelines},
+
+  # Test Instructions:
+  ${data.TestInstructions}
+
+  # GitHub Link:
+  ${data.GitHub}
+
+  # Email:
+  ${data.Email}`
 }
 
 module.exports = generateMarkdown;
